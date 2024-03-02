9cde55931bc2cd36c0241f8c2dcf834bc429feff
SugizakiErika <arsontable119@gmail.com>
2024-03-02 17:57:49 +0900

    ログインと新規登録のbreezeのインストールとCSSが使えるようにコンパイルした

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,17 +9,106 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
+
+/**
+*ホーム画面へ遷移する
+*/
+
+/**
+*ログイン画面へ遷移する
+*/
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+
+//ログインしないと見れない
+Route::middleware(['auth', 'verified'])->group(function (){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+})->name('dashboard');
 
 //カレンダー表示：FullCalendar採用
 //名前はまだ
 Route::get('/calendar', function () {
     return view('calendar');
 });
+
+/**
+*パスワード確認画面へ遷移する
+*/
+
+/**
+*パスワード確認画面へ遷移する
+*/
+
+/**
+*パスワード再設定画面へ遷移する
+*/
+
+/**
+*パスワード登録完了画面へ遷移する
+*/
+
+/**
+*新規作成画面へ遷移する
+*/
+
+/**
+*新規作成完了画面へ遷移する
+*/
+
+/**
+*個人イベント登録画面へ遷移する
+*/
+
+/**
+*個人イベント登録完了画面へ遷移する
+*/
+
+/**
+*通常行事登録画面へ遷移する
+*/
+
+/**
+*通常行事登録完了画面へ遷移する
+*/
+
+/**
+*メイン画面へ遷移する
+*/
+
+/**
+*投稿画面へ遷移する
+*/
+
+/**
+*問い合わせ画面へ遷移する
+*/
+
+/**
+*公開リスト画面へ遷移する
+*/
+
+
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+
+
+
+
