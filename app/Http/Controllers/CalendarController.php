9ab2c7df16c1500962bda5c93b299d073de22728
SugizakiErika <arsontable119@gmail.com>
2024-03-20 13:06:18 +0900

    各イベントの年結合修正と個人イベント登録と問い合わせメールのメール送信部分作成

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,41 +5,52 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Normal_event;
 use App\Models\Diary;
+use App\Models\My_event;
 use Carbon\Carbon;
 
 class CalendarController extends Controller
 {
   // メイン画面を表示するために各テーブルデータを持ってくる
   //各タイトルと日付を持ってきたい
-  public function index(Normal_event $normal_event,Diary $diary)
+  public function index(Normal_event $normal_event,Diary $diary,My_event $my_event)
   {
     $data_diary = $diary->get();
     $data_normal_events = $normal_event->get();
+    $data_my_events = $my_event->get();
     
-    $data_normal_events_vals = [];
     //通常行事(normal_event)に現在の年を結合する
-    foreach($data_normal_events as $value)
-    {
-      $data_normal_events_vals = collect($data_normal_events)->map(function ($data_normal_events) {
-        //○○年の取得きっとあんまりよくない取り方
-        $now = Carbon::now();
-        $now_year = $now->year;
-        //○○年を結合する
-        $data_normal_events['start'] = $now_year."-".$data_normal_events->start;
-        
-        return $data_normal_events;
-      });
-      
-    }
+    $data_normal_events_vals = [];
     
+    $data_normal_events_vals = collect($data_normal_events)->map(function ($data_normal_events) {
+      //○○年の取得きっとあんまりよくない取り方
+      $now = Carbon::now();
+      $now_year = $now->year;
+      //○○年を結合する
+      $data_normal_events['start'] = $now_year."-".$data_normal_events->start;
+        
+      return $data_normal_events;
+    });
+      
+    
+    //個人行事(my_event)に現在の年を結合する
+    $data_my_events_vals = [];
+    
+    $data_my_events_vals = collect($data_my_events)->map(function ($data_my_events) {
+      //○○年の取得きっとあんまりよくない取り方
+      $now = Carbon::now();
+      $now_year = $now->year;
+      //○○年を結合する
+      $data_my_events['start'] = $now_year."-".$data_my_events->start;
+      
+      return $data_my_events;
+    });
+      
+    
+    //dd($data_my_events_vals);
     //concatにて各テーブルの配列を結合する
-    $data = $data_diary->concat($data_normal_events_vals);
+    $data = $data_diary->concat($data_normal_events_vals)->concat($data_my_events_vals);
     //dd($data);
     return view('calendar.index')->with(['data'=> $data]);
   }
   
-  public function send(Request $request)
-  {
-    return view('calendar.create')->with($request);
-  }
 }
